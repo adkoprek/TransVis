ad31a40fa7054dc0c9fb65cc77c610a0333fd3db
Adam Koprek <112286313+adkoprek@users.noreply.github.com>
2026-05-16 13:09:47 +0200

    Rearanged into lib

---
+++ lib/grid_opps.py
@@ -0,0 +1,63 @@
+from typing import Callable
+import numpy as np
+import lib.line_opps as lop
+import lib.svg as svg
+
+
+class Grid:
+    hor = []
+    ver = []
+    trans_hor = []
+    trans_ver = []
+
+    def __init__(self) -> None:
+        pass
+    
+    @staticmethod
+    def create_line(begin_x, end_x, begin_y, end_y, steps) -> lop.PNTS:
+        dx = (end_x - begin_x) / steps
+        dy = (end_y - begin_y) / steps
+
+        line = []
+
+        for i in range(-1, steps + 2):
+            line.append([begin_x + i * dx, begin_y + i * dy])
+
+        return np.array(line)
+
+    def init_grid(self, x_min, x_max, y_min, y_max, num) -> None:
+        dx = int((x_max - x_min) / num)
+        for i in range(x_min, x_max + dx, dx):
+            self.hor.append(self.create_line(x_min, x_max, i, i, dx))
+
+        dy = int((y_max - y_min) / num)
+        for i in range(y_min, y_max + dy, dy):
+            self.ver.append(self.create_line(i, i, x_min, x_max, dy))
+
+    def transform(self, xt: Callable[[float, float], float], yt: Callable[[float, float], float]) -> None:
+        for line in self.hor:
+            self.trans_hor.append(lop.transform(line, xt, yt))
+
+        for line in self.ver:
+            self.trans_ver.append(lop.transform(line, xt, yt))
+
+    def normalize(self, r: int) -> None:
+        m = 0
+        for lines in (self.trans_hor, self.trans_ver):
+            for line in lines:
+                for e in line:
+                    m = max(e[0], m)
+                    m = max(e[1], m)
+
+        for lines in (self.trans_hor, self.trans_ver):
+            for line in lines:
+                line /= m
+                line *= r
+
+    def paint(self, doc: svg.Document, color: str = "#58C4DD") -> None:
+        for lines in (zip(self.hor, self.trans_hor), zip(self.ver, self.trans_ver)):
+            for line, trans in lines:
+                start = lop.construct_bezier_string(lop.line_to_bezier(line))
+                end = lop.construct_bezier_string(lop.line_to_bezier(trans))
+                doc.create_animated_path(start, end, dur=5, color=color)
+
---
+++ lib/line_opps.py
@@ -0,0 +1,101 @@
+from typing import Callable, TypeAlias
+import numpy as np
+import numpy.typing as npt
+
+
+PNT:  TypeAlias = npt.NDArray[np.float64]    # (2,)
+PNTS: TypeAlias = npt.NDArray[np.float64]    # (M,2)
+BEZ:  TypeAlias = npt.NDArray[np.float64]    # (4,2)
+BEZS: TypeAlias = npt.NDArray[np.float64]    # (M,4,2)
+MAT:  TypeAlias = npt.NDArray[np.float64]    # (4,4)
+
+CONVERTER_MATRIX: MAT = (1 / 6) * np.array([[ 0, 6, 0,  0],
+                                            [-1, 6, 1,  0],
+                                            [ 0, 1, 6, -1],
+                                            [ 0, 0, 6,  0]], dtype=np.float64)
+
+
+# This function takes a set of four points (P_{k-1}, P_k, P_{k+1}, P_{k+2}) and
+# constructs a cubic Bezier segment controlled by these points.
+def points_to_bezier(pk_1: PNT, pk: PNT, pk1: PNT, pk2: PNT) -> BEZ:
+    pk_1_x, pk_1_y = pk_1
+    pk_x, pk_y = pk
+    pk1_x, pk1_y = pk1
+    pk2_x, pk2_y = pk2
+
+    bez_x = CONVERTER_MATRIX @ np.array([[pk_1_x], [pk_x], [pk1_x], [pk2_x]])
+    bez_y = CONVERTER_MATRIX @ np.array([[pk_1_y], [pk_y], [pk1_y], [pk2_y]])
+
+    return np.column_stack((bez_x.ravel(), bez_y.ravel()))
+
+
+# This function takes a sequence of 2D points and returns a sequence of cubic
+# Bezier segments constructed from overlapping groups of 4 points.
+def line_to_bezier(line: PNTS) -> BEZS:
+    bezier = []
+    for i in range(1, len(line) - 2):
+        bezier.append(
+            points_to_bezier(
+                line[i - 1],
+                line[i],
+                line[i + 1],
+                line[i + 2],
+            )
+        )
+
+    return np.stack(bezier, axis=0)
+
+
+# Takes a sequence of cubic Bezier segments and returns an SVG path string
+# for the d-attribute of a path element.
+def construct_bezier_string(bezier: BEZS) -> str:
+    r = lambda x: str(np.round(x.item(), 2))
+    bezier_str = f"M {r(bezier[0][0][0])} {r(bezier[0][0][1])} "
+
+    for i in range(len(bezier)):
+        pack = bezier[i]
+        bez = "C "
+
+        for k in range(1, 4):
+            bez += r(pack[k][0]) + " " + r(pack[k][1])
+
+            if k < 3:
+                bez += ", "
+            else:
+                bez += " "
+
+        bezier_str += bez
+
+    return bezier_str
+
+
+# Applies a coordinate-wise transformation to a set of 2D points
+def transform(
+    points: PNTS,
+    xt: Callable[[float, float], float],
+    yt: Callable[[float, float], float]
+) -> PNTS:
+
+    trans_points = []
+
+    for point in points:
+        x, y = point
+        trans_points.append(np.array([xt(x, y), yt(x, y)], dtype=np.float64))
+
+    return np.array(trans_points, dtype=np.float64)
+
+
+# Example transformation
+if __name__ == "__main__":
+    line: PNT = np.array([[0, i] for i in range(-10, 111, 10)], dtype=np.float64)
+
+    print(construct_bezier_string(line_to_bezier(line)))
+
+    trans_line = transform(
+        line,
+        lambda x, y: x + y,
+        lambda _, y: 10 * abs(y) ** 0.5
+    )
+
+    print(construct_bezier_string(line_to_bezier(trans_line)))
+
